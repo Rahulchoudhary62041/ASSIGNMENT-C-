4c42522227bd51158ef63c004fcf74f61fc22230
Rahul Choudhary <128052435+Rahulchoudhary62041@users.noreply.github.com>
2024-01-22 21:03:08 +0530

    Add files via upload

---
+++ compare.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+string str1;
+    string str2 ;
+     cout<<"Enter strin 1 : ";
+cin>>str1;
+    cout<<"Enter strin 1 : ";
+    cin>>str2;
+    if (str1 == str2) {
+        cout << "Strings are equal." << endl;
+    } else {
+        cout << "Strings are not equal." <<endl;
+    }
+
+    return 0;
+}
+
